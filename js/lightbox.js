2a49f34e4df768a874a392d309d532a7b0c5e83b
Jorge Penagos <58637742+A01378450@users.noreply.github.com>
2023-05-12 16:01:11 -0600

    Final
    Version final de la pagina web

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,15 +1,18 @@
+// Jorge Penagos - 12/5/2023
+// Funcionalidad de la lightbox de galeria
+
 const imagenes = document.querySelectorAll('.img-galeria')
 const imagenLight = document.querySelector('.agregar-imagen');
 const contenedorLight = document.querySelector('.imagen-light')
 const closeLight = document.querySelector('.close')
 
-
+// Cambios de clase
 imagenes.forEach(imagen => {
     imagen.addEventListener('click',()=>{
         aparecerImagen(imagen.getAttribute('src'));
     })
 });
-
+// Clicks para cerrar la imagen
 contenedorLight.addEventListener('click',(e)=>{
     if(e.target !== imagenLight){
         contenedorLight.classList.toggle('show')
@@ -18,7 +21,7 @@ contenedorLight.addEventListener('click',(e)=>{
     }
 })
 
-
+// Mostrar imagen con cambio de clase
 const aparecerImagen = (imagen)=>{
     imagenLight.src = imagen;
     contenedorLight.classList.toggle('show')
--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,13 @@
+// Jorge Penagos - 12/5/2023
+// Funcionalidad del menu
 const hamburguer = document.querySelector('.hamburguer')
 const menu = document.querySelector('.menu-navegacion')
 
-
+// Cambios de clase
 hamburguer.addEventListener('click', ()=>{
     menu.classList.toggle("spread")
 })
-
+// Clicks para cerrar el menu
 window.addEventListener('click', e =>{
     if(menu.classList.contains('spread') 
         && e.target != menu && e.target != hamburguer){
